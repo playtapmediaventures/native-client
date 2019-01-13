87a70f66226754e8bbabf6a8885bfa10d93e9425
Ariel Perez <ariel.x.perez@gmail.com>
2019-01-13 00:13:17 -0500

    Configured webpack to not chunk and compress final asset

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,17 @@
 /* eslint no-param-reassign: 0 */
+/* eslint import/no-extraneous-dependencies: 0 */
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = (config, env) => {
-  config.output.filename = 'bundle.js';
-  delete config.devtool;
+  config.output.filename = 'msclvr-native.min.js';
+  delete config.optimization.splitChunks;
+  delete config.optimization.runtimeChunk;
+
+  config.plugins.push(
+    new CompressionPlugin({
+      test: /\.js(\?.*)?$/i
+    })
+  );
 
   return config;
 };
